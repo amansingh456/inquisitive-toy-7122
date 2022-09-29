5586e00b8ef1ca0e12cf8013112b244af3f1d3ac
Aman Singh Rajawat <110046883+amansingh456@users.noreply.github.com>
2022-09-29 22:57:13 +0530

    Delete cart directory
    deleted

--- cart/cart.js
+++
@@ -1,8 +0,0 @@
-import foot from '/fw20_0095_day2/component/foot.js'
-console.log('foot: ', foot);
-
-
-let footer_div = document.getElementById("foot")
-footer_div.innerHTML = foot()
-
-//inquisitive-toy-7122
